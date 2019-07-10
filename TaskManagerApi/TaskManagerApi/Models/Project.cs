08a96d1335937c24f11559a18f6316d798d117d5
Janush P <jmblueprism@gmail.com>
2019-07-10 04:59:52 -0700

    api model added

---
+++ TaskManagerApi/TaskManagerApi/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagerApi.Models
+{
+    public class Project
+    {
+        public int Project_Id { get; set; }
+        public string Name { get; set; }
+        public string Start_Date { get; set; }
+        public string End_Date { get; set; }
+        public int Priority { get; set; }
+    }
+}
