6f2e4571be4f880ca26c976f1f02db9eb71eeddd
Janush P <jmblueprism@gmail.com>
2019-07-15 02:09:15 -0700

    Unit Testing completed

--- TaskManagerApi/TaskManager.Tests/Controllers/TaskManagerControllerTest.cs
+++ TaskManagerApi/TaskManager.Tests/Controllers/TaskManagerControllerTest.cs
@@ -74,5 +74,119 @@ namespace TaskManagerApi.Tests.Controllers
             task = taskManagerController.GetTask(task.TaskId);
             Assert.IsTrue(task.Name == newName);
         }
+
+        [TestMethod]  
+        public void TestAddUser()
+        {
+            TaskManagerController taskManagerController = new TaskManagerController();
+            taskManagerController.AddUser(new Models.User() { FirstName = "Janush", LastName = "P", EmployeeId = "681098" });
+            IEnumerable<User> users = taskManagerController.GetAllUsers();
+            Assert.IsNotNull(users);
+            Assert.IsNotNull(users.Where(t => t.EmployeeId == "681098").First());
+        }
+
+        [TestMethod]
+        public void TestGetUser()
+        {
+            TaskManagerController taskManagerController = new TaskManagerController();
+            taskManagerController.AddUser(new Models.User() { FirstName = "testUser", LastName = "trail", EmployeeId = "empId" });
+            IEnumerable<User> users = taskManagerController.GetAllUsers();
+            Assert.IsNotNull(users);
+            int userId = users.FirstOrDefault().UserId;
+            User user = taskManagerController.GetUser(userId);
+            Assert.IsNotNull(user);
+        }
+
+        [TestMethod]
+        public void TestGetAllUsers()
+        {
+            TaskManagerController taskManagerController = new TaskManagerController();
+            IEnumerable<User> users = taskManagerController.GetAllUsers();
+            Assert.IsNotNull(users);
+        }
+
+        [TestMethod]
+        public void TestDeleteUser()
+        {
+            TaskManagerController taskManagerController = new TaskManagerController();
+            taskManagerController.AddUser(new Models.User() { FirstName = "TestDelete1", LastName = "TestData", EmployeeId = "test123" });
+            IEnumerable<User> users = taskManagerController.GetAllUsers();
+            Assert.IsNotNull(users);
+            int userId = users.Where(t => t.FirstName == "TestDelete1").FirstOrDefault().UserId;
+            User user = taskManagerController.GetUser(userId);
+            taskManagerController.DeleteUser(userId);
+            user = taskManagerController.GetUser(userId);
+            Assert.IsNull(user);
+        }
+
+        [TestMethod]
+        public void TestUpdateUser()
+        {
+            TaskManagerController taskManagerController = new TaskManagerController();
+            taskManagerController.AddUser(new Models.User() { FirstName = "updateUser", LastName = "test", EmployeeId = "empId" });
+            IEnumerable<User> users = taskManagerController.GetAllUsers();
+            Assert.IsNotNull(users);
+            User user = users.Where(t => t.FirstName == "updateUser").FirstOrDefault();
+            string newName = user.FirstName + "Renamed";
+            user.FirstName = newName;
+            taskManagerController.UpdateUser(user);
+            user = taskManagerController.GetUser(user.UserId);
+            Assert.IsTrue(user.FirstName == newName);
+        }
+
+        [TestMethod]
+        public void TestAddProject()
+        {
+            TaskManagerController taskManagerController = new TaskManagerController();
+            taskManagerController.AddProject(new Models.Project() { Name = "AddProject", StartDate = DateTime.Now.ToShortDateString(), EndDate = DateTime.Now.AddDays(10).ToShortDateString(), Priority = 10 });
+            IEnumerable<Project> projects = taskManagerController.GetAllProjects();
+            Assert.IsNotNull(projects);
+            Assert.IsNotNull(projects.Where(t => t.Name == "AddProject").First());
+        }
+
+        [TestMethod]
+        public void TestGetProject()
+        {
+            TaskManagerController taskManagerController = new TaskManagerController();
+            taskManagerController.AddProject(new Models.Project() { Name = "Pro1", StartDate = DateTime.Now.ToShortDateString(), EndDate = DateTime.Now.AddDays(10).ToShortDateString(), Priority = 10 });
+            taskManagerController.AddProject(new Models.Project() { Name = "Pro2", StartDate = DateTime.Now.ToShortDateString(), EndDate = DateTime.Now.AddDays(10).ToShortDateString(), Priority = 10 });
+            IEnumerable<Project> projects = taskManagerController.GetAllProjects();
+            Assert.IsNotNull(projects);
+            int projectId = projects.FirstOrDefault().ProjectId;
+            Project project = taskManagerController.GetProject(projectId);
+            Assert.IsNotNull(project);
+        }
+
+        [TestMethod]
+        public void TestDeleteProject()
+        {
+            TaskManagerController taskManagerController = new TaskManagerController();
+            taskManagerController.AddProject(new Models.Project() { Name = "ProjectDelete", StartDate = DateTime.Now.ToShortDateString(), EndDate = DateTime.Now.AddDays(10).ToShortDateString(), Priority = 10 });
+            IEnumerable<Project> projects = taskManagerController.GetAllProjects();
+            Assert.IsNotNull(projects);
+            int projectId = projects.Where(p => p.Name == "ProjectDelete").FirstOrDefault().ProjectId;
+            taskManagerController.DeleteProject(projectId);
+            Project project = taskManagerController.GetProject(projectId);
+            Assert.IsNull(project);
+        }
+
+        [TestMethod]
+        public void TestUpdateProject()
+        {
+            TaskManagerController taskManagerController = new TaskManagerController();
+            taskManagerController.AddProject(new Models.Project() { Name = "ProjectUpdated", StartDate = DateTime.Now.ToShortDateString(), EndDate = DateTime.Now.AddDays(10).ToShortDateString(), Priority = 10 });
+            IEnumerable<Project> projects = taskManagerController.GetAllProjects();
+            Assert.IsNotNull(projects);
+            Project project = projects.Where(p => p.Name == "ProjectUpdated").FirstOrDefault();
+            string newName = project.Name + "Renamed";
+            project.Name = newName;
+            project.StartDate = DateTime.ParseExact(project.StartDate, "dd/MM/yyyy", CultureInfo.InvariantCulture)
+                        .ToString("MM/dd/yyyy", CultureInfo.InvariantCulture).ToString();
+            project.EndDate = DateTime.ParseExact(project.EndDate, "dd/MM/yyyy", CultureInfo.InvariantCulture)
+                        .ToString("MM/dd/yyyy", CultureInfo.InvariantCulture).ToString();
+            taskManagerController.UpdateProject(project);
+            project = taskManagerController.GetProject(project.ProjectId);
+            Assert.IsTrue(project.Name == newName);
+        }
     }
 }
