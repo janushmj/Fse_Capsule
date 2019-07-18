15d4f3a352095f9ff331cfa710664eba8a8cf6a8
Janush P <jmblueprism@gmail.com>
2019-07-18 08:33:33 -0700

    karma testing

--- AngularProject/src/app/add-project/add-project.component.spec.ts
+++ AngularProject/src/app/add-project/add-project.component.spec.ts
@@ -1,5 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { UpdateTaskComponent } from '../update-task/update-task.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
+import { AppComponent } from '../app.component';
+import { AddTaskComponent } from '../add-task/add-task.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { Ng5SliderModule } from 'ng5-slider';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { AddUserComponent } from '../add-user/add-user.component';
 import { AddProjectComponent } from './add-project.component';
 
 describe('AddProjectComponent', () => {
@@ -8,9 +20,24 @@ describe('AddProjectComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddProjectComponent ]
+      declarations: [AddProjectComponent,
+        AppComponent,
+        AddTaskComponent,
+        AddUserComponent,
+        UpdateTaskComponent],
+        imports: [
+          BrowserModule,
+          AppRoutingModule,
+          HttpClientModule,
+          FormsModule,
+          RouterTestingModule,
+          RouterModule,
+          MDBBootstrapModule,
+          Ng5SliderModule,
+          NgSelectModule
+        ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- AngularProject/src/app/add-user/add-user.component.spec.ts
+++ AngularProject/src/app/add-user/add-user.component.spec.ts
@@ -1,5 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { UpdateTaskComponent } from '../update-task/update-task.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
+import { AppComponent } from '../app.component';
+import { AddTaskComponent } from '../add-task/add-task.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { Ng5SliderModule } from 'ng5-slider';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { AddProjectComponent } from '../add-project/add-project.component';
 import { AddUserComponent } from './add-user.component';
 
 describe('AddUserComponent', () => {
@@ -8,9 +20,24 @@ describe('AddUserComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddUserComponent ]
+      declarations: [AddUserComponent,
+        AppComponent,
+        AddTaskComponent,
+        AddProjectComponent,
+        UpdateTaskComponent],
+        imports: [
+          BrowserModule,
+          AppRoutingModule,
+          HttpClientModule,
+          FormsModule,
+          RouterTestingModule,
+          RouterModule,
+          MDBBootstrapModule,
+          Ng5SliderModule,
+          NgSelectModule
+        ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
